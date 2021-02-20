c772f8c923134ec30812a2cae0d3cd86bd462342
SK SAIFUDDIN <SK SAIFUDDIN@192.168.100.6>
2021-02-20 14:18:00 +0530

    customer registration

---
+++ MVCProjAnno12-CustomerRegistration/src/main/java/com/nt/service/CustomerMgmtServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.dao.ICustomerDAO;
+import com.nt.model.Customer;
+
+@Service("custService")
+public class CustomerMgmtServiceImpl implements ICustomerMgmtService {
+	
+	@Autowired
+	private ICustomerDAO dao;
+
+	@Override
+	public String register(Customer cust) {
+		//used DAO
+		int count=dao.insert(cust);
+		return (count==0)?"Customer Registration Failed":"Customer Registration Sucess";
+	}
+
+}
